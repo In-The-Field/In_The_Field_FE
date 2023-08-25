bcf5ae5745b5d45b359f65f9c33923053b7698d7
Elise Jones <02elise.jones02@gmail.com>
2023-08-25 17:18:49 -0600

    Add logic for handling drag events

--- src/Components/ImageUpload/ImageUpload.js
+++ src/Components/ImageUpload/ImageUpload.js
@@ -1,28 +1,35 @@
-import './ImageUpload.css'
-import { useState } from 'react'
-
-
+import React, { useState } from 'react';
+import "./ImageUpload.css"
 
 const ImageUpload = () => {
-  const [userImage, setUserImage] = useState(null)
+  const [isDragging, setIsDragging] = useState(false);
 
-  const handleUpload = (e) => {
-    const newImage = URL.createObjectURL(e.target.files[0])
-    setUserImage(newImage)
-  }
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if(e.type === "dragenter" || e.type === "dragover"){
+      setIsDragging(true);
+    } else if (e.type === "dragleave"){
+      setIsDragging(false)
+    }
+  };
 
   return (
-    <form id="photo-form"> 
-      <input type="file" id="photo-input" multiple={false} accept="image/*" onChange={handleUpload}/>
-      {/* <label id="label-photo-input">
+    <form id="photo-form" onDragEnter={handleDrag}>
+      <input type="file" id="photo-input" multiple={false} accept="image/*" />
+      <label htmlFor="photo-input" className="label-photo-input">
         <div>
           <p>Drag and Drop your mushroom find</p>
-          <button className="upload-button" onClick={handleUpload}>Upload Photo</button>
+          <button className="upload-button" >
+            Upload Photo
+          </button>
         </div>
-      </label> */}
+      </label>
     </form>
-  )
-}
+  );
+};
+
+export default ImageUpload;
 // const ImageUpload = () => {
 //   const [userImage, setUserImage] = useState(null)
 
@@ -43,5 +50,3 @@ const ImageUpload = () => {
 //     </form>
 //   )
 // }
-
-export default ImageUpload
