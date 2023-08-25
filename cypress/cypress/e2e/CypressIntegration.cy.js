89185e2caae150c301752a7f3024ff5f8f00074b
Joshua Martin <ursa8minority@gmail.com>
2023-08-25 13:39:00 -0600

    modify cypress test

--- cypress/cypress/e2e/CypressIntegration.cy.js
+++ cypress/cypress/e2e/CypressIntegration.cy.js
@@ -1,5 +1,5 @@
-describe("My Test Suite", () => {
-  it("should visit the home page", () => {
-    cy.visit("http://localhost:3000"); 
-  });
-});
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
+})
