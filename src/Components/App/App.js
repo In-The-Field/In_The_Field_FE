11c8e71cfd4232d0c22203146bfd42bcd3d9fab1
Elise Jones <02elise.jones02@gmail.com>
2023-08-25 17:19:47 -0600

    Add logic for imageupload

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,10 @@
 import './App.css';
+import ImageUpload from '../ImageUpload/ImageUpload';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ImageUpload />
     </div>
   );
 }
