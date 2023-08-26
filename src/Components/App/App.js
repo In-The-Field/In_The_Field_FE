49c180e6ceec9db63d72246be39e65a0c79877cb
Joshua Martin <ursa8minority@gmail.com>
2023-08-26 10:27:54 -0600

    update App with correct import routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import './App.css';
-import HomePage from './HomePage'; 
-import ErrorPage from './ErrorPage'; 
+import HomePage from '../HomePage/HomePage'; 
+import ErrorPage from '../ErrorPage/ErrorPage'; 
 
 function App() {
   return (
