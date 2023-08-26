d3e9fe6571be3f16bcea64fa01c89441dba783de
Elise Jones <02elise.jones02@gmail.com>
2023-08-26 17:52:19 -0600

    Create function for changing blob object into url for rendering and add error state

--- src/Components/ImageUpload/ImageUpload.js
+++ src/Components/ImageUpload/ImageUpload.js
@@ -6,7 +6,12 @@ const ImageUpload = () => {
   const [isDragging, setIsDragging] = useState(false);
   const [base64Image, setBase64Image] = useState('')
   const userFileInput = useRef(null)
+  const [error, setError] = useState(null)
 
+  const createUrl = (file) => {
+    return URL.createObjectURL(file)
+  }
+  
   const handleDrag = (e) => {
     e.preventDefault();
     e.stopPropagation();
@@ -21,12 +26,16 @@ const ImageUpload = () => {
     e.preventDefault()
     e.stopPropagation();
     setIsDragging(false)
-    console.log(e.dataTransfer.files[0])
     if (e.dataTransfer.files[0] && e.dataTransfer.files[0].type.includes("image")) {
-      const newImage = URL.createObjectURL(e.dataTransfer.files[0]);
+      const newImage = createUrl(e.dataTransfer.files[0]);
       const createBase64 = btoa(newImage)
       setUserImage(newImage);
       setBase64Image(createBase64)
+      setError(null) 
+    } else {
+      setUserImage(null)
+      setBase64Image(null)
+      setError("This is not an image file please try again")
     }
   }
 
@@ -36,10 +45,15 @@ const ImageUpload = () => {
 
   const handleFileUpload = (e) => {
     if (e.target.files[0] && e.target.files[0].type.includes("image")) {
-      const newImage = URL.createObjectURL(e.target.files[0]);
+      const newImage = createUrl(e.target.files[0]);
       const createBase64 = btoa(newImage)
       setUserImage(newImage);
       setBase64Image(createBase64)
+      setError(null)
+    } else {
+      setUserImage(null)
+      setBase64Image(null)
+      setError("This is not an image file please try again")
     }
   };
 
@@ -48,10 +62,9 @@ const ImageUpload = () => {
       <input type="file" ref={userFileInput} id="photo-input" multiple={false} accept="image/*" onChange={handleFileUpload} />
         <div  className="label-photo-input">
           <p>Drag and Drop your mushroom find</p>
-          <button  className="upload-button" onClick={handleClick} >
-            Upload Photo
-          </button>
+            <button  className="upload-button" onClick={handleClick} >Upload Photo </button>
         </div>
+        <p>{error}</p>
       {userImage && <img src={userImage} />}
       {/* above is only for viewing image droppings change state */}
       {isDragging && <div className="drag-active" onDragEnter={handleDrag} onDragLeave={handleDrag} onDragOver={handleDrag} onDrop={handleDrop}></div> }
