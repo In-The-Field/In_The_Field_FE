b9358bce9eb0ef8ec01ddbadba926f5ac2f4215a
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-27 14:49:14 -0700

    MushroomCard skeleton is started

--- src/Components/MushroomCard/MushroomCard.js
+++ src/Components/MushroomCard/MushroomCard.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import "../MushroomCard.css"
+import ToggleFav from './ToggleFav';
 
-const MushroomCard = () => {
-  return (
-    <div>MushroomCard</div>
-  )
+const MushroomCard = ( {image, latinName, commonNames }) => {
+  const [isFav, setIsFav] = useState(false);
+
+    return (
+        <div className="mushroom-card">
+            <img alt={latinName && commonNames} />
+            {/* need to add src to line 10 */}
+            <div className="mushroom-details">
+                <h2 className="latin-name">{latinName}</h2>
+                <p className="common-name">{commonNames.join(', ')}</p>
+                <ToggleFav isFav={isFav} onToggle={() => setIsFav(!isFav)} />
+            </div>
+        </div>
+    );
 }
 
-export default MushroomCard
+//Depending on how we receive the data from BE, we might not need the .join... if they already give us a string containing all the latin names, that would be perfect
+
+export default MushroomCard;
+
+
+
+
