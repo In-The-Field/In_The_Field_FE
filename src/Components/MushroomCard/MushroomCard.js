e399fd5b84bace56c78cbffca915400991877cb8
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-27 14:53:13 -0700

    added ToggleSave component, moved order or MushroomCard return elements to match wireframe

--- src/Components/MushroomCard/MushroomCard.js
+++ src/Components/MushroomCard/MushroomCard.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import "../MushroomCard.css"
-import ToggleFav from './ToggleFav';
+import ToggleSave from './ToggleFav';
 
 const MushroomCard = ( {image, latinName, commonNames }) => {
   const [isFav, setIsFav] = useState(false);
 
     return (
         <div className="mushroom-card">
+          <div className="name-details">
+                <h2>{latinName}</h2>
+                <p>{commonNames.join(', ')}</p>
+          </div>
             <img alt={latinName && commonNames} />
-            {/* need to add src to line 10 */}
-            <div className="mushroom-details">
-                <h2 className="latin-name">{latinName}</h2>
-                <p className="common-name">{commonNames.join(', ')}</p>
-                <ToggleFav isFav={isFav} onToggle={() => setIsFav(!isFav)} />
-            </div>
+            {/* need to add src to line 14 */}
+            <p>Probability: {probability * 100}%</p>
+              <div className="toggle-container">
+                <ToggleSave isFav={isFav} onToggle={() => setIsFav(!isFav)} />
+              </div>
         </div>
     );
 }
