743ef021627f00e98a34463fa86fd3c13200f6b4
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-27 14:25:37 -0700

    fixed router issues, added MushroomCard component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,29 +1,29 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import './App.css';
-import HomePage from '../HomePage/HomePage'; 
-import ErrorPage from '../ErrorPage/ErrorPage';
-import Nav from '../Nav/Nav';
+import React, { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "../HomePage/HomePage";
+import ErrorPage from "../ErrorPage/ErrorPage";
+import Nav from "../Nav/Nav";
 
 function App() {
   const [showError, setShowError] = useState(false); // State to control error rendering
 
   const handleError = () => {
     setShowError(true);
-  }
+  };
 
   return (
- <>
-   <Nav />
-    {showError && <ErrorPage />}
-    <Router>
+    <>
+      <Nav />
+      {/* {showError && <ErrorPage />} */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/error" element={<ErrorPage />} />
       </Routes>
-    </Router>
-  </>
+    </>
   );
 }
+
+// I think line 18 needs to be in a conditional with a terteary so that the ErrorPage component is only rendered IF there is an error...  let's work on that when the time comes
 
 export default App;
---
+++ src/Components/MushroomCard/MushroomCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../MushroomCard.css"
+
+const MushroomCard = () => {
+  return (
+    <div>MushroomCard</div>
+  )
+}
+
+export default MushroomCard
