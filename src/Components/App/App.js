178c5a0493332a5cb45f090e4d423a271a54c0a0
Joshua Martin <ursa8minority@gmail.com>
2023-08-27 12:55:55 -0600

    conditionally render error page at the top of App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import './App.css';
 import HomePage from '../HomePage/HomePage'; 
-import ErrorPage from '../ErrorPage/ErrorPage'; 
+import ErrorPage from '../ErrorPage/ErrorPage';
 
 function App() {
+  const [showError, setShowError] = useState(false); // State to control error rendering
+
+  const handleError = () => {
+    setShowError(true);
+  }
+
   return (
     <Router>
       <div className="App">
+        {showError && <ErrorPage />}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/error" element={<ErrorPage />} />
