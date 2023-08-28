3f62777ed617eee83a884f4e8337ac150925e68d
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-28 15:58:14 -0700

    changed classNames for userImage & button on homescreenLeft"

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import ImageUpload from '../ImageUpload/ImageUpload';
-import ErrorPage from '../ErrorPage/ErrorPage';
-import './HomePage.css';
+import React from "react";
+import ImageUpload from "../ImageUpload/ImageUpload";
+import ErrorPage from "../ErrorPage/ErrorPage";
+import "./HomePage.css";
+import { useState } from "react"; 
 
 const HomePage = ({ error }) => {
-
   const [userImage, setUserImage] = useState(null);
 
   return (
     <div className="home-page-container">
       <div className="content-container">
-      {error ? (
-          <ErrorPage error={error} /> 
+        {error ? (
+          <ErrorPage error={error} />
         ) : userImage ? (
-          <>
-            <img src={userImage} alt="mushroom" />
-            <button onClick={() => setUserImage(null)}>
-              Upload New Photo
-            </button>
-          </>
+          <div className="homepage-left-container">
+            <img className="user-image" src={userImage} alt="mushroom" />
+            <button className="upload-new-button" onClick={() => setUserImage(null)}>Upload New Photo</button>
+          </div>
         ) : (
           <>
             <h1>Welcome to In The Field!</h1>
             <p>Drag and Drop -or- Upload your mushroom image here!</p>
-            <ImageUpload onImageUpload={setUserImage}  />
+            <ImageUpload onImageUpload={setUserImage} />
           </>
         )}
       </div>
     </div>
   );
-  
 };
 
 export default HomePage;
@@ -38,12 +35,12 @@ export default HomePage;
 
 // const renderMushroomCards = () => {
 //         return mushrooms.map(mushroom => (
-//             <MushroomCard 
+//             <MushroomCard
 //                 key={mushroom.id}
-//                 image={mushroom.image} 
-//                 latinName={mushroom.latinName} 
+//                 image={mushroom.image}
+//                 latinName={mushroom.latinName}
 //                 commonNames={mushroom.commonNames}
-//                 probability={mushroom.probability} 
+//                 probability={mushroom.probability}
 //             />
 //       ));
 // }
