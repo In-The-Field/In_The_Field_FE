33ec83a5fa8ba49850b8f50fa7fb520297659382
Andrea Sorensen <dreasor2987@icloud.com>
2023-08-29 12:50:14 -0700

    moved renderMushroomCards fn into App to pass down as prop

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,11 +4,27 @@ import './App.css';
 import HomePage from '../HomePage/HomePage'; 
 import ErrorPage from '../ErrorPage/ErrorPage';
 import Nav from '../Nav/Nav'
+import { Link } from 'react-router-dom';
+import { mockMushroomCards } from '../../mockData.js';
+import MushroomCard from "../MushroomCard/MushroomCard";
 
 function App() {
   const [showError, setShowError] = useState(false);
   const [error, setError] = useState(null);
   const [userImage, setUserImage] = useState(null);
+
+  const renderMushroomCards = () => {
+    return mockMushroomCards.map(mushroom => (
+      <Link to={`details/${mushroom.id}`} key={mushroom.id}> 
+        <MushroomCard
+            image={mushroom.image}
+            latinName={mushroom.latinName}
+            commonNames={mushroom.commonNames}
+            probability={mushroom.probability}
+        />
+      </Link>
+    ));
+  };
 
    const handleError = (errorInfo) => {
     setError(errorInfo); 
@@ -20,7 +36,7 @@ function App() {
       <Nav />
       {showError && <ErrorPage error={error} />} 
       <Routes>
-        <Route path="/" element={<HomePage error={error}  userImage={userImage} onImageUpload={setUserImage} />} /> 
+        <Route path="/" element={<HomePage error={error}  userImage={userImage} onImageUpload={setUserImage} renderMushroomCards={renderMushroomCards}/>} /> 
         <Route path="/error" element={<ErrorPage error={error} />} /> 
       </Routes>
     </>
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -2,39 +2,27 @@ import React, { useState } from "react";
 import ImageUpload from "../ImageUpload/ImageUpload";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import "./HomePage.css";
-import { mockMushroomCards } from '../../mockData.js';
-import { Link } from 'react-router-dom';
-import MushroomCard from "../MushroomCard/MushroomCard";
 
 
-const HomePage = ({ error, userImage, onImageUpload }) => {
-  // const [mushrooms, setMushrooms] = useState([]);
-
-  const renderMushroomCards = () => {
-    return mockMushroomCards.map(mushroom => (
-      <Link to={`details/${mushroom.id}`} key={mushroom.id}> 
-        <MushroomCard
-            image={mushroom.image}
-            latinName={mushroom.latinName}
-            commonNames={mushroom.commonNames}
-            probability={mushroom.probability}
-        />
-      </Link>
-    ));
-  };
+const HomePage = ({ error, userImage, onImageUpload, renderMushroomCards }) => {
 
   return (
     <div className="home-page-container">
       {userImage ? (
-        <div className="homepage-left-container">
-          <img className="user-image" src={userImage} alt="mushroom" />
-          <button
-            className="upload-new-button"
-            onClick={() => onImageUpload(null)}
-          >
-            Upload New Photo
-          </button>
-        </div>
+        <>
+          <div className="homepage-left-container">
+            <img className="user-image" src={userImage} alt="mushroom" />
+            <button
+              className="upload-new-button"
+              onClick={() => onImageUpload(null)}
+            >
+              Upload New Photo
+            </button>
+          </div>
+          <div className='homepage-right-container'>
+            {renderMushroomCards()}
+          </div>
+        </>
       ) : null}
 
       {!userImage && (
@@ -46,7 +34,6 @@ const HomePage = ({ error, userImage, onImageUpload }) => {
               <h1>Welcome to In The Field!</h1>
               <p>Drag and Drop -or- Upload your mushroom image here!</p>
               <ImageUpload onImageUpload={onImageUpload} />
-              {renderMushroomCards()}
             </>
           )}
         </div>
