6d1209bbdbcbd09e006a13d4f14474e68b0e875c
Joshua Martin <ursa8minority@gmail.com>
2023-08-31 14:21:31 -0600

    fixed details page w/ props being passed down correctly

--- src/Components/DetailsPage/DetailsPage.js
+++ src/Components/DetailsPage/DetailsPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./DetailsPage.css";
 
-const DetailsPage = ({ latinName, commonName, edibility, taxonomy, photo, characteristic }) => {
+const DetailsPage = ({ latinName, commonName, edibility, taxonomy, photo, characteristic, lookalike, psychoactive, description }) => {
   return (
     <div className="details-container">
       <div className="image-latin-common-container">
