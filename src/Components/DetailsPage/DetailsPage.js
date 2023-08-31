03a15a5746cbfa92502febbeec3486ac10922179
Elise Jones <02elise.jones02@gmail.com>
2023-08-30 18:02:22 -0600

    Add characteristics

--- src/Components/DetailsPage/DetailsPage.js
+++ src/Components/DetailsPage/DetailsPage.js
@@ -1,48 +1,46 @@
 import React from "react";
 import "./DetailsPage.css";
-import logo from '../images/logo.png';
 
-const DetailsPage = ({}) => {
+const DetailsPage = ({ latinName, commonName, edibility, taxonomy, photo, characteristic }) => {
   return (
     <div className="details-container">
       <div className="image-latin-common-container">
         <div className="pic">
-          <img className="details-image" src={logo} alt="Logo" />
+          <img className="details-image" src={photo} alt="mushroom" />
         </div>
         <div className="latin-common-edibility-taxonomy">
           <div className="name-taxonomy-cont">
             <div className="name-holder">
-              <h2>Amanita parcivolvata</h2>
-              <p>False Caesar's Mushroom, Ringless False Fly Agaric</p>
+              <h2>{latinName}</h2>
+              <p>{commonName}</p>
             </div>
             <div className="taxonomy">
               <h3>Taxonomy:</h3>
-              <p>Genus: "Amanita",</p>
-              <p>Order: "Agraicales",</p>
-              <p>Family: "Amanitaceae",</p>
-              <p>Phylum: "Basidiomycota",</p>
-              <p> Kingdom: "Fungi"</p>
+              <p>Genus: {taxonomy.genus},</p>
+              <p>Order: {taxonomy.order},</p>
+              <p>Family: {taxonomy.family},</p>
+              <p>Phylum: {taxonomy.phylum},</p>
+              <p> Kingdom: {taxonomy.kingdom}</p>
+            </div>
+            <div className="taxonomy">
+              <h3>Characterisics:</h3>
+              <p> Hymenium Type: {characteristic.hymeniumType},</p>
+              <p> Stipe Character: {characteristic.stipeCharacter},</p>
+              <p> Spore Print Color {characteristic.sporePrintColor},</p>
+              <p> Mushroom Cap Shape: {characteristic.mushroomCapShape},</p>
+              <p> Hymenium Attachment: {characteristic.hymeniumAttachment}</p>
+              <p> Mushroom Ecological Type: {characteristic.mushroomEcologicalType}</p>
             </div>
           </div>
           <div className="info">
-            <p><strong>Edibility: </strong> Edible when cooked</p>
-            <p><strong>Characteristics: </strong>
-              dict - hymenium attachment, hymenium type, mushroom cap shape,
-              mushroom ecological type, spore print color, stipe character
-            </p>
-            <p>
-              <strong>Lookalike: </strong>list of dicts - mushrooms that are frequently confused with the
-              suggested species; each taxon contains entity_id, name and url
-            </p>
-            <p>
-             <strong>Psychoactive: </strong>this mushroom causes poisoning that affects the nervous system
-            </p>
+            <p><strong>Edibility: </strong> {edibility}</p>
+            <p><strong>Lookalike: </strong>{lookalike}</p>
+            <p><strong>Psychoactive: </strong>{psychoactive}</p>
           </div>
         </div>
       </div>
       <div className="description">
-        <p><strong>Description:</strong> {description}
-        </p>
+        <p><strong>Description:</strong>{description}</p>
       </div>
     </div>
   );
