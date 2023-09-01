91022831118685271d83b265e406258d989b8f72
Elise Jones <02elise.jones02@gmail.com>
2023-08-31 22:18:49 -0600

    Adjust flexbox styling

--- src/Components/DetailsPage/DetailsPage.js
+++ src/Components/DetailsPage/DetailsPage.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./DetailsPage.css";
-import { useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
+const DetailsPage = () => {
+  const location = useLocation();
+  const mushroom = location.state;
+  console.log(mushroom);
+  const taxonomy = mushroom.taxonomies[0];
+  //function to handle null fo redibility
+  const characteristics = mushroom.characteristics[0];
+  console.log(characteristics.hymeniumType);
 
-const DetailsPage = ( ) => {
- const location = useLocation()
- const mushroom = location.state
- console.log(mushroom)
- const taxonomy = mushroom.taxonomies[0]
-
- const characteristics = mushroom.characteristics[0]
- console.log(characteristics)
- 
   return (
     <div className="details-container">
       <div className="image-latin-common-container">
@@ -24,34 +23,44 @@ const DetailsPage = ( ) => {
               <h2>{mushroom.latinName}</h2>
               <p>{mushroom.commonName}</p>
             </div>
-            <div className="taxonomy">
-              <h3>Taxonomy:</h3>
-              <p>Genus: {taxonomy.genus},</p>
-              <p>Order: {taxonomy.order},</p>
-              <p>Family: {taxonomy.family},</p>
-              <p>Phylum: {taxonomy.phylum},</p>
-              <p> Kingdom: {taxonomy.kingdom}</p>
+            <div className="tax-charact-cont">
+              <div className="taxonomy">
+                <h3>Taxonomy:</h3>
+                <p>Genus: {taxonomy.genus},</p>
+                <p>Order: {taxonomy.order},</p>
+                <p>Family: {taxonomy.family},</p>
+                <p>Phylum: {taxonomy.phylum},</p>
+                <p> Kingdom: {taxonomy.kingdom}</p>
             </div>
             <div className="taxonomy">
               <h3>Characterisics:</h3>
-              <p> Hymenium Type: {characteristics.hymeniumType},</p>
+              <p> {characteristics.hymeniumType},</p>
               <p> Stipe Character: {characteristics.stipeCharacter},</p>
               <p> Spore Print Color {characteristics.sporePrintColor},</p>
               <p> Mushroom Cap Shape: {characteristics.mushroomCapShape},</p>
               <p> Hymenium Attachment: {characteristics.hymeniumAttachment}</p>
-              <p> Mushroom Ecological Type: {characteristics.mushroomEcologicalType}</p>
+              <p> Mushroom Ecological Type:{characteristics.mushroomEcologicalType}</p>
+              </div>
             </div>
-          </div>
-          <div className="info">
-            <p><strong>Edibility: </strong> {mushroom.edibility}</p>
-            {/* <p><strong>Lookalike: </strong>{mushroom.Alikes}</p> */}
-            <p><strong>Psychoactive: </strong>{mushroom.psychoactive}</p>
           </div>
         </div>
       </div>
+      <div className="info">
+        <p>
+          <strong>Edibility: </strong> {mushroom.edibility}
+        </p>
+        <p><strong>Lookalike: </strong></p>
+        <p>
+          <strong>Psychoactive: </strong>
+          {mushroom.psychoactive}
+        </p>
+      </div>
       <div className="description">
-        <p><strong>Description:</strong>{mushroom.description}</p>
-      </div> 
+        <p>
+          <strong>Description:</strong>
+          {mushroom.description}
+        </p>
+      </div>
     </div>
   );
 };
