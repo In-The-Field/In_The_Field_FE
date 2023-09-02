c0dea2fd28855e844e60d7b6def0b7001d876fd3
Joshua Martin <ursa8minority@gmail.com>
2023-09-02 15:55:06 -0600

    Refactor ToggleSave to persist with state

--- src/Components/FieldGuide/FieldGuide.js
+++ src/Components/FieldGuide/FieldGuide.js
@@ -5,35 +5,41 @@ import { GET_SAVED_USERMUSHROOMS } from "../../queries"
 import { useQuery } from '@apollo/client';
 import ToggleSave from '../ToggleSave/ToggleSave';
 import { NavLink } from 'react-router-dom';
-
+import { useToggleSave } from '../../ToggleSaveContext';
 
 const FieldGuide = () => {
   const userID = 3
-
   const { loading, error: queryError, data } = useQuery(GET_SAVED_USERMUSHROOMS, {
     variables: { id: userID },
   });
-  console.log(data)
+  const { isToggled, toggle } = useToggleSave();
+
+
+
   const renderSavedMushroomCards = () => {
     if (loading) return <p>Loading...</p>;
     // if (!data || !data.mushrooms) return <p>No mushrooms found.</p>;
   console.log(data.user.savedMushrooms)
-    return data.user.savedMushrooms.map((mushroom) => (
-      <div className="mushroom-card-wrapper" key={mushroom.id}>
-        <NavLink to={`details/${mushroom.id}`} className="custom-nav-link" state={mushroom}>
-          <MushroomCard
-            id={mushroom.id}
-            key={mushroom.id}
-            image={mushroom.photo}
-            latinName={mushroom.latinName}
-            commonNames={mushroom.commonName} 
-            // probability={mushroom.probability}
-          />
-        </NavLink>
-        <ToggleSave mushroomId={mushroom.id} isSavedInitially={true} /> 
-      </div>
-    ));
-  };
+  return data.user.savedMushrooms.map((mushroom) => (
+    <div className="mushroom-card-wrapper" key={mushroom.id}>
+      <NavLink to={`details/${mushroom.id}`} className="custom-nav-link" state={mushroom}>
+        <MushroomCard
+          id={mushroom.id}
+          key={mushroom.id}
+          image={mushroom.photo}
+          latinName={mushroom.latinName}
+          commonNames={mushroom.commonName}
+        />
+      </NavLink>
+      <ToggleSave
+        mushroomId={mushroom.id}
+        isSavedInitially={true}
+        onToggle={(mushroomId, isToggled) => toggle()}
+        isToggled={isToggled}
+      />
+    </div>
+  ));
+};
   
   console.log(data)
   //use uqery here get data
